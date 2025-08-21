71bcccc784443535ad0812d7814118b06711719d
Nikolay <kolekdoma@mail.ru>
2025-08-21 20:44:35 +0300

    Add CQRS infrastructure

---
+++ CQRS/ICommand.cs
@@ -0,0 +1,12 @@
+﻿using MediatR;
+
+namespace Simple_Microservice_WebApp.CQRS
+{
+    public interface ICommand : ICommand<Unit>
+    {
+    }
+
+    public interface ICommand<out TResponse> : IRequest<TResponse>
+    {
+    }
+}
